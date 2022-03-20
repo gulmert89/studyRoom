92eba6c71465f996d38e1437e1fcf2f476786368
gulmert89 <gul.mert89@gmail.com>
2022-03-20 17:35:52 +0300

    Update around `Objects` lesson

--- other/basics.js
+++ other/basics.js
@@ -1,8 +1,9 @@
+// MDN (Mozilla Developer Network) is a good source for JS.
 function changeMessages(message, discountRate) {
-    document.getElementById('message').textContent = message;
-    document.getElementById("discount").textContent = discountRate;
+    document.getElementById('message').textContent = message;  // this is a DOM element
+    document.getElementById("discount").textContent = discountRate;  // (Document Object Model)
 }
-
+// another fnc declaration method: let changeMessages = function() { }
 
 let titleText = "Get a grip!",  // Unlike Python, variable names are written as camelCase.
     discount = 40,
@@ -11,7 +12,9 @@ let titleText = "Get a grip!",  // Unlike Python, variable names are written as 
     productRemovalYear = 2025;
 
 discountRate = Number.parseFloat(discountRate)  // Variable updated.
-var discountAvailable = !true  // 'var' is not used anymore. It has bugs.
+var discountAvailable = !true  // 'var' is not used anymore because
+// It has bugs. Its variable defined in a block still lives outside.
+// Also, doesn't throw errors when defined wrongly etc.
 let productId;  // undefined
 productId = 612
 productId = null  // value is wiped.
@@ -48,7 +51,7 @@ changeMessages(
     discount = discountText,
     );
 
-let userProperties = {
+let userProperties = {  // This is an object.
     firstName: "Mert",
     lastName: "Gül",
     age: 33.02,
@@ -59,4 +62,61 @@ console.log(
     `User name: ${userProperties.firstName}\n`,
     typeof userProperties  // output: object (SIMILAR to "dictionaries" in Python, "hashes" in Ruby)
 );
-// MDN (Mozilla Developer Network) is a good source for JS.
+
+
+let price = "19.999"
+price = Number.parseFloat(price);
+
+if (price === NaN) {  // null, undefined, NaN are false.
+    changeMessages("Not NaN Title", typeof price);
+}
+
+else if (+price.toFixed(2) === 20.00) {  // toFixed returns string. We added + as a prefix to make it an integer.
+    changeMessages(
+        `New Price: ${typeof +price.toFixed(1)}`,
+        `Only ${price.toFixed(2)}`
+        );  // btw, toFixed still thinks 'price' is a string :(
+}
+
+// ternary operator
+let errorType = (typeof price === "number") ? "Number error": "Other errors";
+console.log("Error type:", errorType);
+
+/* Difference between '==' and '==='
+    if (1 === "1"): false  ---> Different type! 'Strictly Equal to' or 'Identically Equal to'
+    if (1 == "1"): true  ---> Converts the int to str.
+*/
+
+// loops
+for (let i = 0; i <= 3; i++) {
+    console.log("[INFO]:", i+1, "Mississippi")
+}
+
+let c = 5;
+while (c > 0) {
+    console.log("[INFO]:", c, "Mississippi");
+    c--;
+}
+
+c2 = 10;
+do {
+    console.log("Count down from 10:", c2);  // Runs at least once.
+    c2--;
+} while (c2 >= 15);
+
+// more of objects
+let password = Symbol();
+let person = {
+    account_name: "Mert",
+    age: 33,
+    [password]: "123_!*psswrd",  // information hiding
+    showInfo: function(some_var) {
+        console.log(`You cannot retrieve ${this.account_name}'s password.`);
+        console.log(some_var, "is printed on the console.");
+    }
+};
+person.showInfo("Some Variable");
+console.log(person.password);  // undefined
+console.log(person[password]);  // 123_!*psswrd
+
+
